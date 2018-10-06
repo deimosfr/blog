632acd09349aeb4b14a5bef6313c973071a59f0d
Deimosfr <deimos@deimos.fr>
2018-10-06 14:49:11 +0200

    feat: adding Reatctk js

---
+++ static/js/custom.js
@@ -0,0 +1,8 @@
+let js = document.createElement('script');
+js.src = "https://js.reactk.com/script/reactk.min.js";
+js.async = true;
+js.onload = function(){
+	reactk.init("nPGj3Jeagi8d4LmEhZdh2JYSONxyk");
+};
+var tag = document.getElementsByTagName('head')[0];
+tag.appendChild(js);
