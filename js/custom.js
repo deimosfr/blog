f894e048eb1a50df357dbd688fe3ef273fb98aab
Deimosfr <deimos@deimos.fr>
2018-10-06 16:03:04 +0200

    Publishing to gh-pages (publish.sh)

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,8 @@
-<!-- ReactK Analytics -->
-<script src="https://js.reactk.com/script/reactk.min.js"></script>
-<script type="text/javascript">
-    reactk.init("nPGj3Jeagi8d4LmEhZdh2JYSONxyk");
-</script>
+let js = document.createElement('script');
+js.src = "https://js.reactk.com/script/reactk.min.js";
+js.async = true;
+js.onload = function(){
+	reactk.init("nPGj3Jeagi8d4LmEhZdh2JYSONxyk");
+};
+var tag = document.getElementsByTagName('head')[0];
+tag.appendChild(js);
