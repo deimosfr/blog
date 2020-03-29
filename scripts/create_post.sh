5205bc10d189a2abf2eec5d8a077f6a916f86004
Pierre Mavro <deimos@deimos.fr>
2020-03-29 14:47:58 +0200

    refactor: revert scripts to support only linux

--- scripts/create_post.sh
+++ scripts/create_post.sh
@@ -8,12 +8,12 @@ read logo_image
 
 current_date_minus=$(date "+%Y-%m-%d")
 current_date_slash=$(date "+%Y\/%m\/%d")
-post_name_minus=$(echo ${post_name} | gsed 's/ /-/g' | tr '[:upper:]' '[:lower:]')
+post_name_minus=$(echo ${post_name} | sed 's/ /-/g' | tr '[:upper:]' '[:lower:]')
 filename="${current_date_minus}-${post_name_minus}.md"
 
 cp archetypes/default.md content/post/$filename
-gsed -i "s/DATE_MINUS/${current_date_minus}/g" content/post/$filename
-gsed -i "s/TITLE_MINUS/${post_name_minus}/g" content/post/$filename
-gsed -i "s/TITLE_NORMAL/${post_name}/g" content/post/$filename
-gsed -i "s/DATE_SLASH/${current_date_slash}/g" content/post/$filename
-gsed -i "s/LOGO_IMAGE/${logo_image}/g" content/post/$filename
+sed -i "s/DATE_MINUS/${current_date_minus}/g" content/post/$filename
+sed -i "s/TITLE_MINUS/${post_name_minus}/g" content/post/$filename
+sed -i "s/TITLE_NORMAL/${post_name}/g" content/post/$filename
+sed -i "s/DATE_SLASH/${current_date_slash}/g" content/post/$filename
+sed -i "s/LOGO_IMAGE/${logo_image}/g" content/post/$filename
--- scripts/generate_thumbnails.sh
+++ scripts/generate_thumbnails.sh
@@ -3,10 +3,10 @@
 for page in content/post/*.md ; do
 #page="content/post/2016-01-20-how-we-speed-up-news-feed-and-user-wall-display-using-elasticsearch.md"
 	# Generate thumbnail if defined but not present
-	if [ $(ggrep -c "^thumbnailImage: " $page) == 1 ] ; then
+	if [ $(grep -c "^thumbnailImage: " $page) == 1 ] ; then
 		thumbnail_path="static$(gawk '/^thumbnailImage: /{print $2}' $page)"
 		if [ ! -f "./$thumbnail_path" ] ; then
-			image=$(echo $thumbnail_path | gsed -r 's~.*/(.+)~\1~')
+			image=$(echo $thumbnail_path | sed -r 's~.*/(.+)~\1~')
 			if [ -f "static/images/$image" ] ; then
 				convert static/images/$image -resize 140x140\> $thumbnail_path
 				continue
@@ -17,11 +17,11 @@ for page in content/post/*.md ; do
 	fi
 
 	# Ensure image is set in the page header
-	if [ $(ggrep "^image: " $page | ggrep -c -v http) == 1 ] ; then
-		image=$(gawk '/^image: /{print $2}' $page | gsed -r 's~.*/(.+)~\1~')
+	if [ $(grep "^image: " $page | grep -c -v http) == 1 ] ; then
+		image=$(gawk '/^image: /{print $2}' $page | sed -r 's~.*/(.+)~\1~')
 		thumbnail_path="/static/thumbnails/$image"
-		if [ $(ggrep -c "^thumbnailImage: " $page) == 0 ] ; then
-			gsed -ri "s~^(image:.*)~\1\nthumbnailImage: /thumbnails/$image\nthumbnailImagePosition: left~g" $page
+		if [ $(grep -c "^thumbnailImage: " $page) == 0 ] ; then
+			sed -ri "s~^(image:.*)~\1\nthumbnailImage: /thumbnails/$image\nthumbnailImagePosition: left~g" $page
 		fi
 		if [ ! -f ".$thumbnail_path" ] ; then
 			convert static/images/$image -resize 140x140\> .$thumbnail_path
